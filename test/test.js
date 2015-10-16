3e1f24b0ed29ec89d7eb1e888895b7de3c45b37b
unknown <ryanjsiemens@gmail.com>
2015-10-16 15:23:42 -0700

    added a mock for starting tests.

---
+++ test/test.js
@@ -0,0 +1,24 @@
+var assert = require('assert');
+var burly = require('../Burly.js');
+
+
+describe('Burly', function() {
+  it('Should have a render method', function() {
+    assert.equal('function', typeof burly.Burly.render);
+  });
+
+  describe('render()', function() {
+    it('Should render a template', function() {
+    });
+  });
+});
+
+
+describe('Bind_factory', function() {
+});
+
+
+describe('Bind', function() {
+});
+
+
