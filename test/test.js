5358b4aab10ebf7cb4ce0c50bb516a6cc6c6caea
unknown <ryanjsiemens@gmail.com>
2015-10-28 17:09:36 -0700

    Have a template for testing set and wrote tests for Bind.run()

--- test/test.js
+++ test/test.js
@@ -3,14 +3,34 @@ function restoreState(el, body) {
   el.innerHTML = body;
 }
 
+// Contains tests for utility functions
+QUnit.module("Util", function( hooks ) {
+  QUnit.test("curlyRe()", function( assert ) {
+    assert.equal(
+      "function",
+      typeof Util.curlyRe,
+      "curlyRe is a function"
+    );
 
-// Contains tests for the Burly singleton. 
+    assert.ok(
+        Util.curlyRe("{{ test }}") instanceof Array,
+        "Returns an array when regex matches"
+    );
+
+    assert.ok(
+        Util.curlyRe("{ test }}") === null,
+        "Returns null when regex does not match"
+    );
+  });
+});
+
+
+// Contains tests for the Burly singleton.
 // This is the api that end users will consume.
 QUnit.module("Burly", function( hooks ) {
-
   // setUp before each test
   hooks.beforeEach(function() {
-    this.el = document.getElementById("test-element"); 
+    this.el = document.getElementById("test-element");
     this.data = { testBind: "Successful Bind" };
   });
 
@@ -27,36 +47,35 @@ QUnit.module("Burly", function( hooks ) {
     );
 
     assert.equal(
-      this.el.innerHTML, 
-      "{{ testBind }}", 
-      "View contains a {{ key }} to bind model data to" 
+      this.el.innerHTML,
+      "{{ testBind }}",
+      "View contains a {{ key }} to bind model data to"
     );
 
     Burly.render( "view", this.data );
 
-    assert.equal( this.el.innerHTML, 
-      "Successful Bind", 
-      "Model data bound to a views {{ key }}" 
+    assert.equal(
+      this.el.innerHTML,
+      "Successful Bind",
+      "Model data bound to a views {{ key }}"
     );
 
-    assert.throws( 
+    assert.throws(
       function() {
         Burly.render( "badView", this.data )
-      }, 
-      "undefined scope: badView",
-      "Throws a 'undefined scope:' error" 
-    ); 
+      },
+      "undefined view: badView",
+      "Throws a 'undefined view' error"
+    );
   });
-
 });
 
 
 // Contains test for the Bind constructor
 QUnit.module("Bind", function( hooks ) {
-
   // setUp before each test
   hooks.beforeEach(function() {
-    this.el = document.getElementById("test-element"); 
+    this.el = document.getElementById("test-element");
     this.data = { testBind: "Successful Bind" };
     this.bind = new Bind( "view", this.data );
   });
@@ -67,12 +86,73 @@ QUnit.module("Bind", function( hooks ) {
   });
 
   QUnit.test("run()", function( assert ) {
-    assert.equal( 
-      "function", 
+    assert.equal(
+      "function",
       typeof this.bind.run,
       "Bind contains a run function"
     );
+
+    assert.equal(
+      this.el.innerHTML,
+      "Successful Bind",
+      "Model data bound to a views {{ key }}"
+    );
+
+    assert.equal(
+      this.el,
+      this.bind.root,
+      "Bind sets root to the view name"
+    );
+
+    assert.throws(
+      function() {
+        new Bind( "badView", this.data )
+      },
+      "undefined view: badView",
+      "Throws a 'undefined view' error"
+    );
   });
 
+  QUnit.test("traverse_DOM()", function( assert ) {
+    assert.ok(
+      true,
+      "placeholder"
+    );
+  });
+
+  QUnit.test("node_builder()", function( assert ) {
+    assert.ok(
+      true,
+      "placeholder"
+    );
+  });
+
+  QUnit.test("bind_data()", function( assert ) {
+    assert.ok(
+      true,
+      "placeholder"
+    );
+  });
 });
 
+
+// Contains test for the Bind_factory
+QUnit.module("Bind_factory", function( hooks ) {
+  // setUp before each test
+  hooks.beforeEach(function() {
+    this.el = document.getElementById("test-element");
+    this.data = { testBind: "Successful Bind" };
+  });
+
+  // tearDown after each test
+  hooks.afterEach(function() {
+    restoreState(this.el, "{{ testBind }}");
+  });
+
+  QUnit.test("build()", function( assert ) {
+    assert.ok(
+      true,
+      "placeholder"
+    );
+  });
+});
